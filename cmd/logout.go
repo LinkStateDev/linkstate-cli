c1d1db75004d636b173dd9e11ef10f596189907c
Andrei Protsenko <me@zelfix.nl>
2026-05-24 13:01:30 +0200

    Add logout command

---
+++ cmd/logout.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/LinkStateDev/linkstate-cli/internal/config"
+	"github.com/spf13/cobra"
+)
+
+var logoutCmd = &cobra.Command{
+	Use:   "logout",
+	Short: "Clear saved authentication",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg.Token = ""
+		cfg.Email = ""
+		if err := config.Save(cfg); err != nil {
+			return fmt.Errorf("save config: %w", err)
+		}
+		fmt.Println("Logged out.")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(logoutCmd)
+}
